5d5b3f27be2407eb1d31d9856ef10026cde0a66b
Anthony <pardo.anthony@gmail.com>
2020-04-02 13:30:41 -0500

    modified seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 user1 = User.create(username: "user1", password: "pass1")
-book1 = Book.create(title: "book1", author: "author1", rating: 5, owner: user1)
-book2 = Book.create(title: "book2", author: "author2", rating: 4, owner: user1)
+book1 = Book.create(title: "book1", author: "author1", rating: 5, user: user1)
+book2 = Book.create(title: "book2", author: "author2", rating: 4, user: user1)
