ffe46f48c39da69d3446202634a91331227e6fc9
Anthony <pardo.anthony@gmail.com>
2020-04-02 13:40:22 -0500

    added user_id to books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_180855) do
+ActiveRecord::Schema.define(version: 2020_04_02_183830) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.integer "rating"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
