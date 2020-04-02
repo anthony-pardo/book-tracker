c8163f6c8aa036026d04a5b692deade7f38b86fb
Anthony <pardo.anthony@gmail.com>
2020-04-02 13:26:56 -0500

    created seed data

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+user1 = User.create(username: "user1", password: "pass1")
+book1 = Book.create(title: "book1", author: "author1", rating: 5, owner: user1)
+book2 = Book.create(title: "book2", author: "author2", rating: 4, owner: user1)
