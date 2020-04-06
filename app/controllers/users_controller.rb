e1b0cc1a13ac8438801651d8e9a1a164306964b3
Anthony <pardo.anthony@gmail.com>
2020-04-06 17:13:20 -0500

    added post '/login' route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,15 @@ class UsersController < ApplicationController
     erb :login 
   end
 
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+ 
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
+  end
+
 end
