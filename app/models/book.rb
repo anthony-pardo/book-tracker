2497828cc3bdc36589fce39db274b10b471f079b
Anthony <pardo.anthony@gmail.com>
2020-04-07 17:33:53 -0500

    updated validations for presence of attributes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ActiveRecord::Base
+  validates :title, :author, :rating, presence: true
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  validates :username, :password_digest, presence: true
   has_many :books
 end
